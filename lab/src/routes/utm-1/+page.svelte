47fa8e087a2a00402fc71d6e164f9e04f8e49d0a
David Howell <thinkvisual@hotmail.co.uk>
2023-08-21 17:23:44 +0000

    next idea

--- lab/src/routes/utm-1/+page.svelte
+++ lab/src/routes/utm-1/+page.svelte
@@ -1,20 +1,21 @@
 <script>
+  // Hardcoded values
   let title = "Custom Open Graph Title";
   let description = "This is a custom description for the Open Graph card.";
-  let imageUrl = "/images/test-1.png"; // replace with your image URL
-  let targetUrl = "https://careers.bcg.com/c/consulting-jobs"; // replace with your target URL
+  let imageUrl = "/images/test-1.png"; // Assuming you have an image in your static/images directory
+  let targetUrl = "https://careers.bcg.com/c/consulting-jobs";
+
+  // Redirect to the target URL on page load
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    window.location.href = targetUrl;
+  });
 </script>
 
 <svelte:head>
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
   <meta property="og:image" content={imageUrl} />
-  <meta property="og:url" content={targetUrl} />
 </svelte:head>
-
-<a href={targetUrl}>
-  <img src={imageUrl} alt={title} width="300" />
-  <h2>{title}</h2>
-  <p>{description}</p>
-</a>
 

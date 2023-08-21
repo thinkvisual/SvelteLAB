44b95954d0204fb64c9fde74c933d6dc6fc8ff1e
David Howell <thinkvisual@hotmail.co.uk>
2023-08-21 16:59:36 +0000

    experiment

--- lab/src/routes/utm-1/+page.svelte
+++ lab/src/routes/utm-1/+page.svelte
@@ -1,73 +1,20 @@
 <script>
-    let url = "";
-    let labchannel = "";
-    let labgeo = "";
-    let campaign = "";
-    let utmCode = "";
-    let isUTMGenerated = false;
-  
-    function generateUTMCode() {
-      utmCode = `${url}${labchannel}${labgeo}&utm_content=lab-2023_${campaign}`;
-      isUTMGenerated = true;
-    }
-  
-    async function copyToClipboard() {
-      await navigator.clipboard.writeText(utmCode);
-    }
-  </script>
-  
-  <style>
-    p {
-    word-wrap: break-word;
-    text-transform: lowercase;
-  }
-  </style>
-  
-  <main>
-    <h2>LAB UTM Generator</h2>
-  
-    <form on:submit|preventDefault={generateUTMCode}>
-      <label>
-        Add URL
-        <input type="text" bind:value={url} />
-      </label>
-  
-      <label>
-        Channel
-        <select bind:value={labchannel}>
-          <option value="">Select</option>
-          <option value="?utm_source=linkedin&utm_medium=social&utm_campaign=none&utm_description=organic">LinkedIn | Organic</option>
-          <option value="?utm_source=linkedin&utm_medium=social&utm_campaign=none&utm_description=paid">LinkedIn | Paid</option>
-          <option value="?utm_source=linkedin&utm_medium=social&utm_campaign=none&utm_description=mdp">LinkedIn | MDP</option>
-        </select>
-      </label>
-  
-  
-      <label>
-        Location
-        <select bind:value={labgeo}>
-          <option value="">Select</option>
-          <option value="&utm_geo=lab">LAB</option>
-          <option value="&utm_geo=lon">United Kingdom</option>
-          <option value="&utm_geo=ams">Netherlands</option>
-          <option value="&utm_geo=bru">Belgium</option>
-        </select>
-      </label>
-  
-      <label>
-        Campaign
-        <input type="text" bind:value={campaign}/>
-      </label>
-  
-      {#if !isUTMGenerated}
-        <button type="submit">Generate UTM Code</button>
-      {:else}
-        <button type="button" on:click={copyToClipboard}>Copy UTM Code</button>
-      {/if}
-    </form>
-  
-    {#if utmCode}
-      <p>{utmCode}</p>
-    {/if}
-  </main>
-  
+  let title = "Custom Open Graph Title";
+  let description = "This is a custom description for the Open Graph card.";
+  let imageUrl = "/images/test-1.png"; // replace with your image URL
+  let targetUrl = "https://careers.bcg.com/c/consulting-jobs"; // replace with your target URL
+</script>
+
+<svelte:head>
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={description} />
+  <meta property="og:image" content={imageUrl} />
+  <meta property="og:url" content={targetUrl} />
+</svelte:head>
+
+<a href={targetUrl}>
+  <img src={imageUrl} alt={title} width="300" />
+  <h2>{title}</h2>
+  <p>{description}</p>
+</a>
+
